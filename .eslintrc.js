b0a259e55a7719f634209325c3e5bb8c6b2557d5
ofnullable <ofnullable@gmail.com>
2020-05-17 21:49:23 +0900

    :wrench: Add eslint, prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  trailingComma: 'es5',
+};
